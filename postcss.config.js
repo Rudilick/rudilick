8301695cdaa22ffe223ddf195a2347420e61ca0c
Rudilick <dnffpr2@gmail.com>
2025-06-05 01:38:52 +0900

    fix: Tailwind v4 PostCSS 설정 반영

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 // postcss.config.js
 module.exports = {
   plugins: {
-    '@tailwindcss/postcss': {},  // ✅ 요걸로 교체
+    '@tailwindcss/postcss': {}, // ← 기존 'tailwindcss'가 아니라 이걸 사용해야 함
     autoprefixer: {},
   },
 }
